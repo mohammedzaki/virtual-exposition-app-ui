6b62b7d7114e3c990c965af285958af23f8a03a2
Mohammed Zaki <mohammedzaki.dev@gmail.com>
2017-08-16 07:02:31 +0200

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,5 @@ module.exports = function(grunt) {
 
   grunt.registerTask('heroku', ['build']);
   
-  grunt.registerTask('heroku', function (target) {
-  // use the target to do whatever, for example:
-  grunt.task.run('build:' + target);
-});
 
 };
